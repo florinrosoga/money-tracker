b9ce9ab75bd1142973800a1e08d073f3c4acb068
Florin Rosoga <florin.rosoga@gmail.com>
2024-08-23 12:23:16 +0300

    form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ function App() {
     <div>
 <main>
   <h1>$400<span>.00</span></h1>
+  <form>
+  <div className='basic'>
+  <input type="text" placeholder={'+200 new samsungtv'}/>"
+  <input type="datetime-local" />"
+  </div>
+<div className='description'>
+    <input type="text" />"
+    </div>
+    <button type='submit'>Add new transaction</button>
+  </form>
+  <div className='transactions'>
+    <div className='transaction'>
+
+      
+    </div>
+  </div>
 </main>
     </div>
   );
