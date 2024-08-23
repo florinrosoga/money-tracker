6fc989cac4569c1eaca46838c364e15fe901f597
Florin Rosoga <florin.rosoga@gmail.com>
2024-08-23 12:32:42 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-<main>
-  <h1>$400<span>.00</span></h1>
-  <form>
-  <div className='basic'>
-  <input type="text" placeholder={'+200 new samsungtv'}/>"
-  <input type="datetime-local" />"
-  </div>
-<div className='description'>
-    <input type="text" />"
-    </div>
-    <button type='submit'>Add new transaction</button>
-  </form>
-  <div className='transactions'>
-    <div className='transaction'>
+      <main>
+        <h1>
+          $400<span>.00</span>
+        </h1>
+        <form>
+          <div className="basic">
+            <input type="text" placeholder={"+200 new samsungtv"} />"
+            <input type="datetime-local" />"
+          </div>
+          <div className="description">
+            <input type="text" />"
+          </div>
+          <button type="submit">Add new transaction</button>
+        </form>
+        <div className="transactions">
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New Samsung</div>
+              <div className="sescription">It was time for a new TV</div>
+            </div>
+            <div className="right">
+              <div className="price">$500</div>
+              <div className="datetime">2022-12-18 15:45</div>
+            </div>
+          </div>
+        </div>
 
-      
-    </div>
-  </div>
-</main>
+        <div className="transactions">
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New Samsung</div>
+              <div className="sescription">It was time for a new TV</div>
+            </div>
+            <div className="right">
+              <div className="price">$500</div>
+              <div className="datetime">2022-12-18 15:45</div>
+            </div>
+          </div>
+        </div>
+        <div className="transactions">
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New Samsung</div>
+              <div className="sescription">It was time for a new TV</div>
+            </div>
+            <div className="right">
+              <div className="price">$500</div>
+              <div className="datetime">2022-12-18 15:45</div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
