6f13408cca718b289cb3ca6b031aa803bd7e59da
Florin Rosoga <florin.rosoga@gmail.com>
2024-08-23 12:54:56 +0300

    css

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         </h1>
         <form>
           <div className="basic">
-            <input type="text" placeholder={"+200 new samsungtv"} />"
-            <input type="datetime-local" />"
+            <input type="text" placeholder={"+200 new samsungtv"} />
+            <input type="datetime-local" />
           </div>
           <div className="description">
             <input type="text" placeholder="description" />
@@ -21,10 +21,10 @@ function App() {
           <div className="transaction">
             <div className="left">
               <div className="name">New Samsung</div>
-              <div className="sescription">It was time for a new TV</div>
+              <div className="description">It was time for a new TV</div>
             </div>
             <div className="right">
-              <div className="price">$500</div>
+              <div className="price red">-$500</div>
               <div className="datetime">2022-12-18 15:45</div>
             </div>
           </div>
@@ -33,11 +33,11 @@ function App() {
         <div className="transactions">
           <div className="transaction">
             <div className="left">
-              <div className="name">New Samsung</div>
-              <div className="sescription">It was time for a new TV</div>
+              <div className="name">Gog Job New Website</div>
+              <div className="description">It was time for a new TV</div>
             </div>
             <div className="right">
-              <div className="price">$500</div>
+              <div className="price green">+$900</div>
               <div className="datetime">2022-12-18 15:45</div>
             </div>
           </div>
@@ -45,11 +45,11 @@ function App() {
         <div className="transactions">
           <div className="transaction">
             <div className="left">
-              <div className="name">New Samsung</div>
-              <div className="sescription">It was time for a new TV</div>
+              <div className="name">Iphone</div>
+              <div className="description">It was time for a new TV</div>
             </div>
             <div className="right">
-              <div className="price">$500</div>
+              <div className="price red">-$900</div>
               <div className="datetime">2022-12-18 15:45</div>
             </div>
           </div>
