4abf655a198759f9b51e2a14b427a43e903f463e
Florin Rosoga <florin.rosoga@gmail.com>
2024-08-24 13:08:00 +0300

    ups

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const Transaction = require("./models/Transaction.js");
+const {default: mongoose} = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
@@ -10,8 +13,12 @@ app.get("/api/test", (req, res) => {
   res.json({message: "it works"}); // Updated message
 });
 
-app.post("/api/transaction", (req, res) => {
-  res.json(req.body);
+app.post("/api/transaction", async (req, res) => {
+  await mongoose.connect("process.env.MONGO_URL");
+  const {name, description, daytime} = req.body;
+  const transaction = await Transaction.create({name, description, datetime});
+
+  res.json(transaction);
 });
 
 // Define the port the server will listen on
